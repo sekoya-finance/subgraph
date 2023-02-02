d9871113d13af07fb0572b62616dc8b1581f3b29
HHK <hhk.contact@protonmail.com>
2023-02-02 16:50:34 -0600

    feat: add factory

--- src/factory.ts
+++ src/factory.ts
@@ -2,7 +2,7 @@ import { CreateDCA } from '../generated/Factory/Factory';
 import { Factory } from '../generated/schema';
 import { createVault } from './vault';
 
-const FACTORY = '';
+const FACTORY = '0xE832e7b348215A87507b1EaF0ea4c2EFb29dDe00';
 
 export function getOrCreateFactory(): Factory {
   let factory = Factory.load(FACTORY);
